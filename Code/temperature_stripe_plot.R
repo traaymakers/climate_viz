52d1938850fd02071ee32163b5ce72fa54e96121
Raaymakers <t.m.raaymakers@nvwa.onmicrosoft.com>
2022-05-30 10:13:34 +0200

    pdf > png

--- Code/temperature_stripe_plot.R
+++ Code/temperature_stripe_plot.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(scales)
+library(glue)
 
 t_data <- read.csv("data/GLB.Ts+dSST.csv", skip = 1, na = "***") %>% 
   select(year = Year, t_diff = 'J.D') %>% 
@@ -12,4 +13,16 @@ t_data %>%
                     values = rescale(c(min(t_data$t_diff), 0, max(t_data$t_diff))),
                     n.breaks = 12) +
   coord_cartesian(expand = FALSE) +
-  theme_void()
+  scale_x_continuous(breaks = seq(1890, 2020, 30)) +
+  labs(title = glue("Global temperature change ({min(t_data$year)}-{max(t_data$year)})")) +
+  theme_void() +
+  theme(
+    axis.text.x = element_text(color = "white", 
+                               margin =margin(t=5, b=10, unit ="pt")),
+    plot.title = element_text(color = "white", 
+                               margin =margin(b=5, t=10, unit ="pt"),
+                              hjust = 0.05),
+    plot.background = element_rect(fill = "black")
+  )
+
+ggsave("Figures/warming_stripes.png", width = 8, height = 4.5)
