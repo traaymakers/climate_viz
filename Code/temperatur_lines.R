17d99e0f2caeeb10c5501c149310458a756132e2
Raaymakers <t.m.raaymakers@nvwa.onmicrosoft.com>
2022-06-02 15:26:16 +0200

    new code and figure

--- Code/temperatur_lines.R
+++ Code/temperatur_lines.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(glue)
 
 t_diff <- read.csv("data/GLB.Ts+dSST.csv", skip = 1, na = "***") %>% 
   select(year = Year, month.abb) %>% 
@@ -7,16 +8,60 @@ t_diff <- read.csv("data/GLB.Ts+dSST.csv", skip = 1, na = "***") %>%
 
 last_dec <- t_diff %>% 
   filter(month == "Dec") %>% 
-  mutate(year = year -1, 
+  mutate(year = year +1, 
          month = "last_Dec")
 
 next_jan <- t_diff %>% 
   filter(month == "Jan") %>% 
-  mutate(year = year +1, 
+  mutate(year = year -1, 
          month = "next_Jan")
 
-bind_rows(last_dec, t_diff, next_jan) %>%  
+t_data <- bind_rows(last_dec, t_diff, next_jan) %>%  
   mutate(month = factor(month,
-                        levels = c("last_Dec", month.abb, "next_Jan"))) %>% 
-  ggplot(aes(x=month, y=t_diff, group=year, color=year)) +
-  geom_line()
+                        levels = c("last_Dec", month.abb, "next_Jan")),
+         month_number = as.numeric(month) -1,
+         this_year = year == 2022) 
+
+annotation <- t_data %>% 
+  slice_max(year) %>% 
+  slice_max(month_number)
+
+t_data %>% 
+  ggplot(aes(x=month_number, y=t_diff, group=year, color=year,
+             size = this_year)) +
+  geom_hline(yintercept = 0, color="white") +
+  geom_line() +
+  geom_text(data = annotation,
+            aes(x=month_number, y = t_diff, label = year, color = year), inherit.aes = FALSE,
+            hjust = 0, size = 5, nudge_x = 0.15, fontface = "bold" ) +
+  scale_x_continuous(breaks = 1:12,
+                     labels=month.abb,
+                     sec.axis = dup_axis(name = NULL, labels = NULL)) +
+  scale_y_continuous(breaks = seq(-2, 2, 0.2),
+                     sec.axis = dup_axis(name = NULL, labels = NULL)) +
+  scale_size_manual(breaks = c(FALSE, TRUE),
+                    values = c(0.25, 1),
+                    guide = "none") +
+  scale_color_viridis_c(breaks = seq(1880, 2020, 20),
+                        guide = guide_colorbar(frame.colour = "white",
+                                               frame.linewidth = 1.5)) +
+  coord_cartesian(xlim = c(1,12)) +
+  labs(x = NULL, 
+       y = "Temperature change since pre-industrial time [\u00B0C]",
+       title = glue("Global temperature change since {min(t_data$year)} by month")) +
+  theme(
+    panel.background = element_rect(fill = "black", color = "white", size = 1),
+    plot.background = element_rect(fill = "#444444"),
+    panel.grid = element_blank(),
+    axis.text = element_text(color = "white", size = 13),
+    axis.ticks = element_line(color = "white"),
+    axis.title = element_text(color = "white", size = 13),
+    axis.ticks.length = unit(-5, "pt"),
+    plot.title = element_text(color = "white", hjust = 0.5, size = 15),
+    legend.title = element_blank(),
+    legend.background = element_rect(fill = NA),
+    legend.text = element_text(color = "white"),
+    legend.key.height = unit(55, "pt")
+  )
+
+ggsave("Figures/temperature_lines.pdf", width = 8, height = 4.5)
