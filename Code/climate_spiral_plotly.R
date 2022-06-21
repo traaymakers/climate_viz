f7fae8281dfd6b025de2cd9dbf3e1d951e1b3499
Raaymakers <t.m.raaymakers@nvwa.onmicrosoft.com>
2022-06-21 15:49:24 +0200

    plotly climate spiral

---
+++ Code/climate_spiral_plotly.R
@@ -0,0 +1,50 @@
+library(tidyverse)
+library(plotly)
+library(glue)
+
+t_data <- read.csv("Data/GLB.Ts+dSST.csv", skip = 1, na = "***") %>% 
+  select(year = Year, all_of(month.abb)) %>% 
+  pivot_longer(-year, names_to = "month", values_to = "t_diff") %>% 
+  drop_na() %>% 
+  mutate(month = factor(month, levels = month.abb)) %>% #change month to factor, otherwise it will be sorted alphabetically
+  arrange(year, month) %>% 
+  mutate(month_number = as.numeric(month),
+         radius = t_diff + 1.5, #+1.5 to get rid of negative radius values
+         theta = 2 * pi *(month_number-1)/12,
+         x = radius * sin(theta),
+         y = radius * cos(theta),
+         z = year,
+         label = glue("{month} {year}\n{t_diff}"))
+
+# t_data %>% 
+#   ggplot(aes(x=x, y=y, color=z)) +
+#   geom_path()
+
+axx <- list(
+  title = "",
+  showgrid = FALSE,
+  zeroline = FALSE
+)
+
+axy <- list(
+  title = "",
+  showgrid = FALSE,
+  zeroline = FALSE
+)
+
+axz <- list(
+  title = "Year"
+)
+
+
+plot_ly(t_data, x = ~x, y = ~y, z = ~z, text = ~label,
+        hoverinfo = "text",
+        type = 'scatter3d', mode = 'lines',
+        line = list(width = 4, color = ~t_diff,
+                    cmid = 0, cmin = min(t_data$t_diff), cmax = max(t_data$t_diff),
+                    colorscale = list(c(0,'#0000FF'),
+                                       c(0.5, "#FFFFFF"),
+                                       c(1,'#FF0000')))) %>% 
+layout(scene = list(xaxis=axx,
+                    yaxis=axy,
+                    zaxis=axz))
